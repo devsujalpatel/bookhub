6c3f6e7ab06fc760c198b4b3ea3a452548dd7e81
devsujalpatel <sujal7455@gmail.com>
2025-03-26 04:31:44 +0530

    ui done

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-async function getData() {
-    const url = "https://api.freeapi.app/api/v1/public/books";
-    try {
-        const response = await fetch(url)
-        if(!response.ok){
-            throw new Error(`Response status${response.status}`)
-        }
-        const jsonData = await response.json()
-        console.log(jsonData.data.data[0]);
+let jsonData;
 
-    } catch (error) {
-        console.log(error.message);
+(async function getData() {
+  const url = "https://api.freeapi.app/api/v1/public/books";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status${response.status}`);
     }
+    jsonData = await response.json();
+  } catch (error) {
+    console.log(error.message);
   }
-  
+})();
 
-  getData();
+const library = document.getElementById("library")
